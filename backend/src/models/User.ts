3c4051574518559d33bff1e6fcd9dbda5307bbd4
matpitas <pitas.matheus4@gmail.com>
2024-03-05 08:02:23 -0300

    First Commit

---
+++ backend/src/models/User.ts
@@ -0,0 +1,12 @@
+import { __Type } from "graphql";
+import { Field, ID, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class User {
+
+    @Field(__Type => ID)
+    id: string
+
+    @Field()
+    name: string
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,36 @@
+import { gql, useQuery } from '@apollo/client'
+import { NewUserForm } from './components/new-user-form'
+
+type User = {
+  id: string,
+  name: string
+}
+
+export const GET_USER = gql`
+  query {
+    users {
+       id
+       name
+    }
+  }
+`
+
+function App() {
+
+  const { data, loading } = useQuery<{ users: User[]}>(GET_USER)
+
+  if(loading) {
+    return <p>Carregando...</p>
+  }
+
+  return (
+    <>
+      <ul>
+        {data?.users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+      <NewUserForm />
+    </>
+  )
+}
+
+export default App
